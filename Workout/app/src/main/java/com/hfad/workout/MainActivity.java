cb14adf1425837c3b857554388f2a01140110478
Róbert Ďuriančík <r.duriancik@gmail.com>
2016-12-06 19:19:27 +0100

    MainActivity getFragment

--- Workout/app/src/main/java/com/hfad/workout/MainActivity.java
+++ Workout/app/src/main/java/com/hfad/workout/MainActivity.java
@@ -1,13 +1,19 @@
 package com.hfad.workout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WorkoutDetailFragment frag = (WorkoutDetailFragment)
+                getFragmentManager().findFragmentById(R.id.detail_fragment);
+
+        frag.setWorkout(1);
     }
 }
---
+++ Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -0,0 +1,34 @@
+package com.hfad.workout;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WorkoutDetailFragment extends Fragment {
+
+    private long workoutId;
+
+    public void setWorkout(long workoutId) {
+        this.workoutId = workoutId;
+    }
+
+    public WorkoutDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+}
