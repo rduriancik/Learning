839516be0a55ee83f1c7447582762e84ee836485
Róbert Ďuriančík <r.duriancik@gmail.com>
2016-12-08 23:35:06 +0100

    WorkoutDetailFragment complete

--- Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
+++ Workout/app/src/main/java/com/hfad/workout/WorkoutDetailFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -31,4 +32,17 @@ public class WorkoutDetailFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescripion());
+
+        }
+    }
 }
