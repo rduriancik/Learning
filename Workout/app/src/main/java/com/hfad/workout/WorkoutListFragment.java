d6dfb3332266b17d25ba87f7df8487da571086a0
Róbert Ďuriančík <r.duriancik@gmail.com>
2016-12-08 23:37:21 +0100

    WorkoutListFragment

--- Workout/app/src/main/java/com/hfad/workout/WorkoutListFragment.java
+++ Workout/app/src/main/java/com/hfad/workout/WorkoutListFragment.java
@@ -2,16 +2,13 @@ package com.hfad.workout;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class WorkoutListFragment extends Fragment {
+public class WorkoutListFragment extends ListFragment {
 
 
     public WorkoutListFragment() {
@@ -22,9 +19,7 @@ public class WorkoutListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
 }
