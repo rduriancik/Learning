e20b30a330679faa9e83e8a1d20d0b29d0488736
Róbert Ďuriančík <r.duriancik@gmail.com>
2016-12-08 23:44:23 +0100

    WokroutListFragment complete

--- Workout/app/src/main/java/com/hfad/workout/WorkoutListFragment.java
+++ Workout/app/src/main/java/com/hfad/workout/WorkoutListFragment.java
@@ -6,6 +6,7 @@ import android.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 public class WorkoutListFragment extends ListFragment {
@@ -19,6 +20,15 @@ public class WorkoutListFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] names = new String[Workout.workouts.length];
+        for(int i = 0; i < names.length; ++i) {
+            names[i] = Workout.workouts[i].getName();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                inflater.getContext(), android.R.layout.simple_list_item_1, names);
+        setListAdapter(adapter);
+
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
