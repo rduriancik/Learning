bf6cd26eb5663822c8b8fc07ccc03943586f03e8
Róbert Ďuriančík <r.duriancik@gmail.com>
2016-12-29 14:45:50 +0100

    PizzaFragment

---
+++ BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/PizzaFragment.java
@@ -0,0 +1,30 @@
+package com.hfad.bitsandpizzas;
+
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class PizzaFragment extends ListFragment {
+
+
+    public PizzaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (inflater.getContext(), android.R.layout.simple_list_item_1,
+                        getResources().getStringArray(R.array.pizzas));
+        setListAdapter(arrayAdapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
