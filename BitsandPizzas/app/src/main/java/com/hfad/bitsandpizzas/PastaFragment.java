e16a9482198c981160cd2db6d8a38c3fea8cf801
Róbert Ďuriančík <r.duriancik@gmail.com>
2016-12-29 14:50:43 +0100

    PastaFragment

--- BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/PastaFragment.java
+++ BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/PastaFragment.java
@@ -7,20 +7,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class PizzaFragment extends ListFragment {
+public class PastaFragment extends ListFragment {
 
-    public PizzaFragment() {
+    public PastaFragment() {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
                 (inflater.getContext(), android.R.layout.simple_list_item_1,
-                        getResources().getStringArray(R.array.pizzas));
+                        getResources().getStringArray(R.array.pasta));
         setListAdapter(arrayAdapter);
+
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
