964ea6f431151f65c65148512397fc4cee37c82b
Róbert Ďuriančík <r.duriancik@gmail.com>
2016-12-29 14:53:48 +0100

    StoresFragment

--- BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/StoresFragment.java
+++ BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/StoresFragment.java
@@ -1,6 +1,5 @@
 package com.hfad.bitsandpizzas;
 
-
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
@@ -8,19 +7,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
-public class PastaFragment extends ListFragment {
+public class StoresFragment extends ListFragment {
 
-    public PastaFragment() {
+    public StoresFragment() {
         // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
                 (inflater.getContext(), android.R.layout.simple_list_item_1,
-                        getResources().getStringArray(R.array.pasta));
+                        getResources().getStringArray(R.array.stores));
         setListAdapter(arrayAdapter);
 
         return super.onCreateView(inflater, container, savedInstanceState);
