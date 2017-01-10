643e2887bbce2b5c2df9e11de354fd8a9d9b4213
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-01-10 13:13:15 +0100

    Button Click Counter MainActivity

--- Master_Android_N_App_Development_With_Java/ButtonClickCounter/app/src/main/java/com/example/buttonclickcounter/MainActivity.java
+++ Master_Android_N_App_Development_With_Java/ButtonClickCounter/app/src/main/java/com/example/buttonclickcounter/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.buttonclickcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
     }
 }
