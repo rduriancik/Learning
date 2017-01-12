7bfe1624d5bc456c856bde2242548dd7c6745eae
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-01-12 23:49:09 +0100

    Button Click Counter MainActivity

--- Master_Android_N_App_Development_With_Java/ButtonClickCounter/app/src/main/java/com/example/buttonclickcounter/MainActivity.java
+++ Master_Android_N_App_Development_With_Java/ButtonClickCounter/app/src/main/java/com/example/buttonclickcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.buttonclickcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,5 +22,17 @@ public class MainActivity extends AppCompatActivity {
         userInput = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
+        textView.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String result = userInput.getText().toString();
+                result += '\n';
+                textView.append(result);
+                userInput.setText("");
+            }
+        };
+        button.setOnClickListener(ourOnClickListener);
     }
 }
