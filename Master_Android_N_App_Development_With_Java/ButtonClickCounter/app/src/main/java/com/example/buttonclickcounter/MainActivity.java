e8fcaee9c5b7a1c777814f72109ce86afdeb8bbc
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-01-13 10:59:19 +0100

    Button Click Counter - activity lifecycle

--- Master_Android_N_App_Development_With_Java/ButtonClickCounter/app/src/main/java/com/example/buttonclickcounter/MainActivity.java
+++ Master_Android_N_App_Development_With_Java/ButtonClickCounter/app/src/main/java/com/example/buttonclickcounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.buttonclickcounter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,16 +12,18 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private EditText userInput;
-    private Button button;
     private TextView textView;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         userInput = (EditText) findViewById(R.id.editText);
-        button = (Button) findViewById(R.id.button);
+        Button button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
         textView.setText("");
         textView.setMovementMethod(new ScrollingMovementMethod());
@@ -34,5 +37,63 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         button.setOnClickListener(ourOnClickListener);
+
+        Log.d(TAG, "onCreate: out");
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: in");
+        super.onStart();
+        Log.d(TAG, "onStart: out");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState: out");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: in");
+        super.onResume();
+        Log.d(TAG, "onResume: out");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: in");
+        super.onPause();
+        Log.d(TAG, "onPause: out");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: out");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: in");
+        super.onStop();
+        Log.d(TAG, "onStop: out");
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart: in");
+        super.onRestart();
+        Log.d(TAG, "onRestart: out");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: in");
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: out");
     }
 }
