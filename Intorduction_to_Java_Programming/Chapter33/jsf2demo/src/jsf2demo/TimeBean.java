5bad4c05dba79abce27da7f24d6aadd9c94431f4
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-01-19 15:33:23 +0100

    jsf2demo TimeBean

--- Intorduction_to_Java_Programming/Chapter33/jsf2demo/src/jsf2demo/TimeBean.java
+++ Intorduction_to_Java_Programming/Chapter33/jsf2demo/src/jsf2demo/TimeBean.java
@@ -1,11 +1,15 @@
+package jsf2demo;
+
 /**
  * Created by robert on 18.1.2017.
  */
 
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.inject.Named;
 
-@Named
+@ManagedBean
+@Named(value = "timeBean")
 @RequestScoped
 public class TimeBean {
     public TimeBean() {
