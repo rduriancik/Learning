6e36373fd839e2a7d3c63f9541b01d83b92af051
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-02-05 10:50:38 +0100

    Youtube Player - YoutubePlayer.OnInitializedListener impl

--- Master_Android_N_App_Development_With_Java/YoutubePlayer/app/src/main/java/com/example/robertduriancik/youtubeplayer/YoutubeActivity.java
+++ Master_Android_N_App_Development_With_Java/YoutubePlayer/app/src/main/java/com/example/robertduriancik/youtubeplayer/YoutubeActivity.java
@@ -2,7 +2,9 @@ package com.example.robertduriancik.youtubeplayer;
 
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.util.Log;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
@@ -11,6 +13,8 @@ import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity
         implements YouTubePlayer.OnInitializedListener {
+
+    private static final String TAG = "YoutubeActivity";
 
     static final String GOOGLE_API_KEY = "AIzaSyAkxy2iFkZ0UHoAPqzNugRT5rjS-Ve5adU";
     static final String YOUTUBE_VIDEO_ID = "-6w5KTZx6qE";
@@ -24,19 +28,37 @@ public class YoutubeActivity extends YouTubeBaseActivity
         ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
         setContentView(layout);
 
-        YouTubePlayerView player = new YouTubePlayerView(this);
-        player.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+        YouTubePlayerView playerView = new YouTubePlayerView(this);
+        playerView.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT));
-        layout.addView(player);
+        layout.addView(playerView);
+        playerView.initialize(GOOGLE_API_KEY, this);
     }
 
     @Override
-    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer,
+                                        boolean wasRestored) {
+        Log.d(TAG, "onInitializationSuccess: provider is " + provider.getClass().toString());
+        Toast.makeText(this, "Initialized Youtube Player successfully", Toast.LENGTH_LONG).show();
 
+        if (!wasRestored) {
+            youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID);
+        }
     }
 
     @Override
-    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult youTubeInitializationResult) {
+        final int REQUEST_CODE = 1;
 
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show();
+        } else {
+            String errorMessage = String.format("There was an error initializing the YoutubePlayer (%1$s)",
+                    youTubeInitializationResult.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
     }
+
+
 }
