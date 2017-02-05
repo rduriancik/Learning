eb528aff9afd380edd84ae51e8dad98f2da32531
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-02-05 15:02:01 +0100

    Youtube Player - autoplay, completed

--- Master_Android_N_App_Development_With_Java/YoutubePlayer/app/src/main/java/com/example/robertduriancik/youtubeplayer/StandaloneActivity.java
+++ Master_Android_N_App_Development_With_Java/YoutubePlayer/app/src/main/java/com/example/robertduriancik/youtubeplayer/StandaloneActivity.java
@@ -33,11 +33,11 @@ public class StandaloneActivity extends AppCompatActivity implements View.OnClic
         switch (view.getId()) {
             case R.id.btnPlayVideo:
                 intent = YouTubeStandalonePlayer.createVideoIntent(this,
-                        YoutubeActivity.GOOGLE_API_KEY, YoutubeActivity.YOUTUBE_VIDEO_ID);
+                        YoutubeActivity.GOOGLE_API_KEY, YoutubeActivity.YOUTUBE_VIDEO_ID, 0, true, false);
                 break;
             case R.id.btnPlayList:
                 intent = YouTubeStandalonePlayer.createPlaylistIntent(this,
-                        YoutubeActivity.GOOGLE_API_KEY, YoutubeActivity.YOUTUBE_PLAYLIST);
+                        YoutubeActivity.GOOGLE_API_KEY, YoutubeActivity.YOUTUBE_PLAYLIST, 0, 0, true, true);
                 break;
             default:
                 // Nothing
