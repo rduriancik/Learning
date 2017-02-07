a899cd04b4d998a17e4c3c8ea9119f98fe0b9cc5
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-02-07 12:40:27 +0100

    GetRawData class - doInBackground method, DownloadStatus enum

---
+++ Master_Android_N_App_Development_With_Java/FlickrBrowser/app/src/main/java/com/example/robertduriancik/flickrbrowser/GetRawData.java
@@ -0,0 +1,89 @@
+package com.example.robertduriancik.flickrbrowser;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+enum DownloadStatus {IDLE, PROCESSING, NOT_INITIALISED, FAILED_OR_EMPTY, OK}
+
+/**
+ * Created by robert on 7.2.2017.
+ */
+
+class GetRawData extends AsyncTask<String, Void, String> {
+    private static final String TAG = "GetRawData";
+
+    private DownloadStatus mDownloadStatus;
+
+    public GetRawData() {
+        mDownloadStatus = DownloadStatus.IDLE;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+
+        if (strings == null) {
+            mDownloadStatus = DownloadStatus.NOT_INITIALISED;
+            return null;
+        }
+
+        try {
+            mDownloadStatus = DownloadStatus.PROCESSING;
+            URL url = new URL(strings[0]);
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int response = connection.getResponseCode();
+            Log.d(TAG, "doInBackground: The response code was " + response);
+
+            StringBuilder result = new StringBuilder();
+
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+//            String line;
+//            while (null != (line = reader.readLine())) {
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                result.append(line).append("\n");
+            }
+
+            mDownloadStatus = DownloadStatus.OK;
+            return result.toString();
+
+        } catch (MalformedURLException ex) {
+            Log.e(TAG, "doInBackground: Invalid URL " + ex.getMessage());
+        } catch (IOException ex) {
+            Log.e(TAG, "doInBackground: IOException reading data: " + ex.getMessage());
+        } catch (SecurityException ex) {
+            Log.e(TAG, "doInBackground: Security Exception. Needs permission? " + ex.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException ex) {
+                    Log.e(TAG, "doInBackground: Error closing stream " + ex.getMessage());
+                }
+            }
+        }
+
+        mDownloadStatus = DownloadStatus.FAILED_OR_EMPTY;
+        return null;
+    }
+}
