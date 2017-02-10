9b48e3fa6b5914a90528ba8d48e7075e448015ea
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-02-10 13:44:03 +0100

    FlickrRecyclerViewAdapter update

--- Master_Android_N_App_Development_With_Java/FlickrBrowser/app/src/main/java/com/example/robertduriancik/flickrbrowser/FlickrRecyclerViewAdapter.java
+++ Master_Android_N_App_Development_With_Java/FlickrBrowser/app/src/main/java/com/example/robertduriancik/flickrbrowser/FlickrRecyclerViewAdapter.java
@@ -3,9 +3,13 @@ package com.example.robertduriancik.flickrbrowser;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -21,6 +25,42 @@ class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewA
     public FlickrRecyclerViewAdapter(Context context, List<Photo> photoList) {
         mContext = context;
         mPhotoList = photoList;
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d(TAG, "getItemCount: called");
+        return ((mPhotoList != null) && (mPhotoList.size() != 0) ? mPhotoList.size() : 0);
+    }
+
+    void loadNewData(List<Photo> newPhotos) {
+        mPhotoList = newPhotos;
+        notifyDataSetChanged();
+    }
+
+    public Photo getPhoto(int position) {
+        return ((mPhotoList != null) && (mPhotoList.size() != 0) ? mPhotoList.get(position) : null);
+    }
+
+    @Override
+    public FlickrImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Called by the layout manager when it needs new view
+        Log.d(TAG, "onCreateViewHolder: new view requested");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.browse, parent, false);
+        return new FlickrImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(FlickrImageViewHolder holder, int position) {
+        // Called by the layout manager when it wants new data in an existing row
+        Photo photoItem = mPhotoList.get(position);
+        Log.d(TAG, "onBindViewHolder: " + photoItem.getTitle() + " --> " + position);
+        Picasso.with(mContext).load(photoItem.getImage())
+                .error(R.drawable.image_broken)
+                .placeholder(R.drawable.placeholder)
+                .into(holder.thumbnail);
+
+        holder.title.setText(photoItem.getTitle());
     }
 
     static class FlickrImageViewHolder extends RecyclerView.ViewHolder {
