04c909e1a773297af4c81e05320018a59df417b5
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-02-10 12:32:50 +0100

    FlickrRecyclerViewAdapter, FickrImageViewHolder

---
+++ Master_Android_N_App_Development_With_Java/FlickrBrowser/app/src/main/java/com/example/robertduriancik/flickrbrowser/FlickrRecyclerViewAdapter.java
@@ -0,0 +1,38 @@
+package com.example.robertduriancik.flickrbrowser;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by robert on 10.2.2017.
+ */
+
+class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewAdapter.FlickrImageViewHolder> {
+    private static final String TAG = "FlickrRecyclerViewAdapt";
+    private List<Photo> mPhotoList;
+    private Context mContext;
+
+    public FlickrRecyclerViewAdapter(Context context, List<Photo> photoList) {
+        mContext = context;
+        mPhotoList = photoList;
+    }
+
+    static class FlickrImageViewHolder extends RecyclerView.ViewHolder {
+        private static final String TAG = "FlickrImageViewHolder";
+        ImageView thumbnail = null;
+        TextView title = null;
+
+        public FlickrImageViewHolder(View itemView) {
+            super(itemView);
+            Log.d(TAG, "FlickrImageViewHolder: starts");
+            this.thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
+            this.title = (TextView) itemView.findViewById(R.id.title);
+        }
+    }
+}
