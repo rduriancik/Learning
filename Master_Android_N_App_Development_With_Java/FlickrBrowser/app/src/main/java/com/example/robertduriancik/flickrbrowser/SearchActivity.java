12bd7c80460d516d7c17742cd52ed55dc2ac6a48
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-02-13 22:22:28 +0100

    SearchActivity

--- Master_Android_N_App_Development_With_Java/FlickrBrowser/app/src/main/java/com/example/robertduriancik/flickrbrowser/SearchActivity.java
+++ Master_Android_N_App_Development_With_Java/FlickrBrowser/app/src/main/java/com/example/robertduriancik/flickrbrowser/SearchActivity.java
@@ -1,24 +1,59 @@
 package com.example.robertduriancik.flickrbrowser;
 
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.SearchView;
 
 public class SearchActivity extends BaseActivity {
+    private static final String TAG = "SearchActivity";
 
     private SearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: starts");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
         activateToolbar(true);
+        Log.d(TAG, "onCreate: ends");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu: starts");
         getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        mSearchView = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        mSearchView.setSearchableInfo(searchableInfo);
+//        Log.d(TAG, "onCreateOptionsMenu: " + getComponentName().toString());
+//        Log.d(TAG, "onCreateOptionsMenu: hint is " + mSearchView.getQueryHint());
+//        Log.d(TAG, "onCreateOptionsMenu: searchable info is " + searchableInfo.toString());
+
+        mSearchView.setIconified(false);
+
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Log.d(TAG, "onQueryTextSubmit: called");
+                mSearchView.clearFocus();
+                finish();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        Log.d(TAG, "onCreateOptionsMenu: returned " + true);
         return true;
     }
 }
