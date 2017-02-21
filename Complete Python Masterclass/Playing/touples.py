55091c1207df7faa5487e6fcc3bfd0e362c2de0f
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-02-21 18:21:10 +0100

    touples

--- Complete Python Masterclass/Playing/touples.py
+++ Complete Python Masterclass/Playing/touples.py
@@ -27,3 +27,24 @@ metallica2 = ["Ride the Lightning", "Metallica", 1984]
 print(metallica2)
 metallica2[0] = "Master of Puppets"  # Can change field, because it's a list
 print(metallica2)
+
+print()
+
+title, artist, year = imelda
+print(title)
+print(artist)
+print(year)
+
+print()
+
+imelda = "More Mayhem", "Emilda May", 2011, (1, "Pulling the Rug"), (2, "Psycho"), (3, "Mayhem"), (
+    4, "Kentish Town Waltz")
+print(imelda)
+title, artist, year, track1, track2, track3, track4 = imelda
+print(title)
+print(artist)
+print(year)
+print(track1)
+print(track2)
+print(track3)
+print(track4)
