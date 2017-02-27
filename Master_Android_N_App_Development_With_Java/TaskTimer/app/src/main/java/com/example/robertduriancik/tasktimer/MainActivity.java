5a8f9c43c2aba8aadc7e6cfaeb777bb70083b0dd
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-02-27 22:04:09 +0100

    menu_main.xml

--- Master_Android_N_App_Development_With_Java/TaskTimer/app/src/main/java/com/example/robertduriancik/tasktimer/MainActivity.java
+++ Master_Android_N_App_Development_With_Java/TaskTimer/app/src/main/java/com/example/robertduriancik/tasktimer/MainActivity.java
@@ -103,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menumain_settings) {
             return true;
         }
 
