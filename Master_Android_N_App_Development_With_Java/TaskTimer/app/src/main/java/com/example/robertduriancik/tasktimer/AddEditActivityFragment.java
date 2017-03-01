64aa37eaad148934d8074495eebc8525d9794ed4
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-03-01 22:25:54 +0100

    AddEditActivityFragment

--- Master_Android_N_App_Development_With_Java/TaskTimer/app/src/main/java/com/example/robertduriancik/tasktimer/AddEditActivityFragment.java
+++ Master_Android_N_App_Development_With_Java/TaskTimer/app/src/main/java/com/example/robertduriancik/tasktimer/AddEditActivityFragment.java
@@ -1,11 +1,15 @@
 package com.example.robertduriancik.tasktimer;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -13,14 +17,103 @@ import android.view.ViewGroup;
 public class AddEditActivityFragment extends Fragment {
     private static final String TAG = "AddEditActivityFragment";
 
+    public enum FragmentEditMode {EDIT, ADD}
+
+    private FragmentEditMode mMode;
+
+    private EditText mNameTextView;
+    private EditText mDescriptionTextView;
+    private EditText mSortOrderTextView;
+    private Button mSaveButton;
+
     public AddEditActivityFragment() {
         Log.d(TAG, "AddEditActivityFragment: constructor called");
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: starts");
-        return inflater.inflate(R.layout.fragment_add_edit, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_add_edit, container, false);
+
+        mNameTextView = (EditText) view.findViewById(R.id.addedit_name);
+        mDescriptionTextView = (EditText) view.findViewById(R.id.addedit_description);
+        mSortOrderTextView = (EditText) view.findViewById(R.id.addedit_sortorder);
+        mSaveButton = (Button) view.findViewById(R.id.addedit_save);
+
+        Bundle arguments = getActivity().getIntent().getExtras();
+
+        final Task task;
+        if (arguments != null) {
+            Log.d(TAG, "onCreateView: retrieving task details");
+
+            task = (Task) arguments.getSerializable(Task.class.getSimpleName());
+            if (task != null) {
+                Log.d(TAG, "onCreateView: Task details found, editing...");
+                mNameTextView.setText(task.getName());
+                mDescriptionTextView.setText(task.getDescription());
+                mSortOrderTextView.setText(Integer.toString(task.getSortOrder()));
+                mMode = FragmentEditMode.EDIT;
+            } else {
+                mMode = FragmentEditMode.ADD;
+            }
+        } else {
+            task = null;
+            Log.d(TAG, "onCreateView: No arguments, adding new record");
+            mMode = FragmentEditMode.ADD;
+        }
+
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Update the database if at least one field has changed
+                int so; // save to repeated conversions to int
+                if (mSortOrderTextView.length() > 0) {
+                    so = Integer.parseInt(mSortOrderTextView.getText().toString());
+                } else {
+                    so = 0;
+                }
+
+                ContentResolver contentResolver = getActivity().getContentResolver();
+                ContentValues contentValues = new ContentValues();
+
+                switch (mMode) {
+                    case EDIT:
+                        if (!mNameTextView.getText().toString().equals(task.getName())) {
+                            contentValues.put(TasksContract.Columns.TASKS_NAME, mNameTextView.getText().toString());
+                        }
+
+                        if (!mDescriptionTextView.getText().toString().equals(task.getDescription())) {
+                            contentValues.put(TasksContract.Columns.TASKS_DESCRIPTION, mDescriptionTextView.getText().toString());
+                        }
+
+                        if (so != task.getSortOrder()) {
+                            contentValues.put(TasksContract.Columns.TASKS_SORTORDER, so);
+                        }
+
+                        if (contentValues.size() != 0) {
+                            Log.d(TAG, "onClick: updating task");
+                            contentResolver.update(TasksContract.buildTaskUri(task.getId()), contentValues, null, null);
+                        }
+                        break;
+                    case ADD:
+                        if (mNameTextView.length() > 0) {
+                            Log.d(TAG, "onClick: adding new task");
+                            contentValues.put(TasksContract.Columns.TASKS_NAME, mNameTextView.getText().toString());
+                            contentValues.put(TasksContract.Columns.TASKS_DESCRIPTION, mDescriptionTextView.getText().toString());
+                            contentValues.put(TasksContract.Columns.TASKS_SORTORDER, so);
+                            contentResolver.insert(TasksContract.CONTENT_URI, contentValues);
+                        }
+                        break;
+                }
+
+                Log.d(TAG, "onClick: Done editing");
+            }
+        });
+
+        Log.d(TAG, "onCreateView: exit");
+
+        return view;
     }
 }
