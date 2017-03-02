2642cbc3a4eaa67a0ecd02acd79df37d0ce4ee56
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-03-02 22:43:30 +0100

    implementing LoaderManager.LoaderCallbacks interface

--- Master_Android_N_App_Development_With_Java/TaskTimer/app/src/main/java/com/example/robertduriancik/tasktimer/MainActivityFragment.java
+++ Master_Android_N_App_Development_With_Java/TaskTimer/app/src/main/java/com/example/robertduriancik/tasktimer/MainActivityFragment.java
@@ -1,22 +1,76 @@
 package com.example.robertduriancik.tasktimer;
 
-import android.support.v4.app.Fragment;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.security.InvalidParameterException;
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final String TAG = "MainActivityFragment";
+
+    public static final int LOADER_ID = 0;
 
     public MainActivityFragment() {
+        Log.d(TAG, "MainActivityFragment: starts");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: starts");
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Log.d(TAG, "onCreateLoader: starts with id " + id);
+        String[] projection = {TasksContract.Columns._ID, TasksContract.Columns.TASKS_NAME,
+                TasksContract.Columns.TASKS_DESCRIPTION, TasksContract.Columns.TASKS_SORTORDER};
+        String sortOrder = TasksContract.Columns.TASKS_SORTORDER + "," + TasksContract.Columns.TASKS_NAME;
+
+        switch (id) {
+            case LOADER_ID:
+                return new CursorLoader(getActivity(),
+                        TasksContract.CONTENT_URI,
+                        projection,
+                        null,
+                        null,
+                        sortOrder);
+            default:
+                throw new InvalidParameterException(TAG + ".onCreateLoader called with invalid loader id " + id);
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Log.d(TAG, "onLoadFinished: starts");
+        int count = -1;
+
+        if (data != null) {
+            while (data.moveToNext()) {
+                for (int i = 0; i < data.getColumnCount(); ++i) {
+                    Log.d(TAG, "onLoadFinished: " + data.getColumnName(i) + ": " + data.getString(i));
+                }
+                Log.d(TAG, "onLoadFinished: ==================================================");
+            }
+            count = data.getCount();
+        }
+        Log.d(TAG, "onLoadFinished: count is " + count);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        Log.d(TAG, "onLoaderReset: start");
     }
 }
