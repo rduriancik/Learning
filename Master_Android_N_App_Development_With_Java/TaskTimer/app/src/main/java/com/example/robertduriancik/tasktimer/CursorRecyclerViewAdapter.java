0695ff4b28bb94805393500abe1a2cbf72f4da6d
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-03-06 20:37:21 +0100

    CursorRecyclerViewAdapter - swapCursor

--- Master_Android_N_App_Development_With_Java/TaskTimer/app/src/main/java/com/example/robertduriancik/tasktimer/CursorRecyclerViewAdapter.java
+++ Master_Android_N_App_Development_With_Java/TaskTimer/app/src/main/java/com/example/robertduriancik/tasktimer/CursorRecyclerViewAdapter.java
@@ -60,6 +60,33 @@ class CursorRecyclerViewAdapter extends RecyclerView.Adapter<CursorRecyclerViewA
         }
     }
 
+    /**
+     * Swap in a new Cursor, returning the old Cursor
+     * The returned old Cursor is <em>not</em> closed.
+     *
+     * @param newCursor The new Cursor to be used
+     * @return Returns the previously set Cursor, or null if there wasn't one.
+     * If the given new Cursor is the same instance as the previously set
+     * Cursor, null is also returned
+     */
+    Cursor swapCursor(Cursor newCursor) {
+        if (newCursor == mCursor) {
+            return null;
+        }
+
+        final Cursor oldCursor = mCursor;
+        mCursor = newCursor;
+        if (newCursor != null) {
+            // notify the observers about the new cursor
+            notifyDataSetChanged();
+        } else {
+            // notify the observers about the lack of a data set
+            notifyItemRangeRemoved(0, getItemCount());
+        }
+
+        return oldCursor;
+    }
+
     static class TaskViewHolder extends RecyclerView.ViewHolder {
         private static final String TAG = "TaskViewHolder";
 
