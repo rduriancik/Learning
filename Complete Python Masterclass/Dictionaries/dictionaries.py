cf810d5bebcc9eedf45485551a0e30f14857b9a3
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-03-08 23:26:44 +0100

    dictionaries

--- Complete Python Masterclass/Dictionaries/dictionaries.py
+++ Complete Python Masterclass/Dictionaries/dictionaries.py
@@ -2,7 +2,32 @@ fruit = {"orange": "a sweet, orange, citrus fruit",
          "apple": "good for making cider",
          "lemon": "a sour, yellow citrus fruit",
          "grape": "a small, sweet fruit growing in bunches",
-         "lime": "a sour, green citrus fruit"}
+         "lime": "a sour, green citrus fruit"}  # ,
+# "apple": "round and crunchy"}  # updated apple value
 
 print(fruit)
 print(fruit["lemon"])
+
+fruit["pear"] = "an odd shape apple"
+print(fruit)
+
+fruit["lime"] = "great with tequila"
+print(fruit)
+
+# print(fruit["tomato"]) # throws error
+
+while True:
+    dict_key = input("Please enter a fruit: ")
+    if dict_key == "quit":
+        break;
+    if dict_key in fruit:
+        description = fruit.get(dict_key)  # doesn't throw error if key doesn't exist
+        print(description)
+    else:
+        print("We don't have a " + dict_key)
+
+del fruit["lemon"]
+print(fruit)
+# del fruit -> delete fruit variable
+fruit.clear()  # clears content
+print(fruit)
--- Complete Python Masterclass/Dictionaries/motorbike.py
+++ Complete Python Masterclass/Dictionaries/motorbike.py
@@ -3,4 +3,5 @@ bike = {"make": "Honda",
         "color": "red",
         "engine_size": 250}
 
-print(bike)
+print(bike["engine_size"])
+print(bike["color"])
