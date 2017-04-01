831afea6afc4c62c438b4a0724e0a50e2102df08
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-04-01 22:35:34 +0200

    Toast message

--- KotlinHelloWorld/app/src/main/java/com/example/robertduriancik/kotlinhelloworld/MainActivity.kt
+++ KotlinHelloWorld/app/src/main/java/com/example/robertduriancik/kotlinhelloworld/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.robertduriancik.kotlinhelloworld
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener { toast("Message: ${input.text}")}
     }
 }
