072993c84b9fe0fa869b1a9c2a149a069a385323
Róbert Ďuriančík <r.duriancik@gmail.com>
2017-04-03 23:38:23 +0200

    MainActivity.java -  implements DialogEvents

--- Master_Android_N_App_Development_With_Java/TaskTimer/app/src/main/java/com/example/robertduriancik/tasktimer/MainActivity.java
+++ Master_Android_N_App_Development_With_Java/TaskTimer/app/src/main/java/com/example/robertduriancik/tasktimer/MainActivity.java
@@ -11,7 +11,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements CursorRecyclerViewAdapter.OnTaskClickListener,
-        AddEditActivityFragment.OnSaveClicked {
+        AddEditActivityFragment.OnSaveClicked,
+        AppDialog.DialogEvents {
     private static final String TAG = "MainActivity";
 
     // Whether or not the activity is in 2-pane mode
@@ -125,5 +126,20 @@ public class MainActivity extends AppCompatActivity implements CursorRecyclerVie
                 startActivity(detailIntent);
             }
         }
+    }
+
+    @Override
+    public void onPositiveDialogResult(int dialogId, Bundle args) {
+        Log.d(TAG, "onPositiveDialogResult: called");
+    }
+
+    @Override
+    public void onNegativeDialogResult(int dialogId, Bundle args) {
+        Log.d(TAG, "onNegativeDialogResult: called");
+    }
+
+    @Override
+    public void onDialogCancelled(int dialogId) {
+        Log.d(TAG, "onDialogCancelled: called");
     }
 }
