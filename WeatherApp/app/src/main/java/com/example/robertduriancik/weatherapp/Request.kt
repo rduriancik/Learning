e87ab1f7a63c0da53eda94ecbd63a2a742bb395e
rduriancik <r.duriancik@gmail.com>
2017-04-20 23:53:28 +0200

    Request class, extension functions

---
+++ WeatherApp/app/src/main/java/com/example/robertduriancik/weatherapp/Request.kt
@@ -0,0 +1,16 @@
+package com.example.robertduriancik.weatherapp
+
+import android.util.Log
+import java.net.URL
+
+
+/**
+ * Created by robert-ntb on 4/20/17.
+ */
+
+class Request(val url: String) {
+    fun run() {
+        val forecastJsonStr = URL(url).readText()
+        Log.d(javaClass.simpleName, forecastJsonStr)
+    }
+}
--- WeatherApp/app/src/main/java/com/example/robertduriancik/weatherapp/activities/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/robertduriancik/weatherapp/activities/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.example.robertduriancik.weatherapp.R
 import com.example.robertduriancik.weatherapp.adapters.ForecastListAdapter
+import org.jetbrains.anko.find
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
     }
