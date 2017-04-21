ce27ff83da07f99b4b55ba15284c6c11c2223f05
rduriancik <r.duriancik@gmail.com>
2017-04-22 00:14:30 +0200

    ForecastDataMapper

---
+++ WeatherApp/app/src/main/java/com/example/robertduriancik/weatherapp/domain/ForecastDataMapper.kt
@@ -0,0 +1,37 @@
+package com.example.robertduriancik.weatherapp.domain
+
+import com.example.robertduriancik.weatherapp.data.Forecast
+import com.example.robertduriancik.weatherapp.data.ForecastResult
+import java.text.DateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+import com.example.robertduriancik.weatherapp.domain.Forecast as ModelForecast
+
+/**
+ * Created by robert-ntb on 4/21/17.
+ */
+
+class ForecastDataMapper {
+
+    fun convertFromDataModel(forecast: ForecastResult): ForecastList {
+        return ForecastList(forecast.city.name, forecast.city.country,
+                convertForecastListToDomain(forecast.list))
+    }
+
+    private fun convertForecastListToDomain(list: List<Forecast>): List<ModelForecast> {
+        return list.mapIndexed { index, forecast ->
+            val dt = Calendar.getInstance().timeInMillis + TimeUnit.DAYS.toMillis(index.toLong())
+            convertForecastItemToDomain(forecast.copy(dt = dt))
+        }
+    }
+
+    private fun convertForecastItemToDomain(forecast: Forecast): ModelForecast {
+        return ModelForecast(convertDate(forecast.dt), forecast.weather[0].description,
+                forecast.temp.max.toInt(), forecast.temp.min.toInt())
+    }
+
+    private fun convertDate(date: Long): String {
+        val df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault())
+        return df.format(date)
+    }
+}
