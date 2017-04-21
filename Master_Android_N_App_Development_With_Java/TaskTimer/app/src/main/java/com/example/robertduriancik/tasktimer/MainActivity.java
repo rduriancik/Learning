0173f2a4f0ab77c0d6c844bdf6b00ce3b76a8623
rduriancik <r.duriancik@gmail.com>
2017-04-21 23:02:35 +0200

    showAboutDialog() method created and called in onOptionsItemSelected()

--- Master_Android_N_App_Development_With_Java/TaskTimer/app/src/main/java/com/example/robertduriancik/tasktimer/MainActivity.java
+++ Master_Android_N_App_Development_With_Java/TaskTimer/app/src/main/java/com/example/robertduriancik/tasktimer/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.robertduriancik.tasktimer;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,6 +11,8 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import static com.example.robertduriancik.tasktimer.AppDialog.DIALOG_ID;
 
@@ -23,6 +27,9 @@ public class MainActivity extends AppCompatActivity implements CursorRecyclerVie
 
     public static final int DIALOG_ID_DELETE = 1;
     public static final int DIALOG_ID_CANCEL_EDIT = 2;
+
+    private AlertDialog mDialog = null; // module scope because we need to dismiss it in onStop
+    // e.g. when orientation changes, to avoid memory leaks.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,12 +81,30 @@ public class MainActivity extends AppCompatActivity implements CursorRecyclerVie
             case R.id.menumain_settings:
                 break;
             case R.id.menumain_showAbout:
+                showAboutDialog();
                 break;
             case R.id.menumain_generate:
                 break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void showAboutDialog() {
+        @SuppressLint("InflateParams") View messageView = getLayoutInflater().inflate(R.layout.about, null, false);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(messageView);
+
+        mDialog = builder.create();
+        mDialog.setCanceledOnTouchOutside(true);
+        mDialog.setTitle(R.string.app_name);
+        mDialog.setIcon(R.mipmap.ic_launcher);
+
+        TextView textView = (TextView) messageView.findViewById(R.id.about_version);
+        textView.setText("v" + BuildConfig.VERSION_NAME);
+
+        mDialog.show();
     }
 
     @Override
