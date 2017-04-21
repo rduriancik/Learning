3fe707786b4347d2dff496a724425ad78522da78
rduriancik <r.duriancik@gmail.com>
2017-04-21 23:57:37 +0200

    Domain classes

---
+++ WeatherApp/app/src/main/java/com/example/robertduriancik/weatherapp/domain/DomainClasses.kt
@@ -0,0 +1,11 @@
+package com.example.robertduriancik.weatherapp.domain
+
+/**
+ * Created by robert-ntb on 4/21/17.
+ */
+
+data class ForecastList(val city: String, val country: String,
+                        val dailyForecast: List<Forecast>)
+
+data class Forecast(val date: String, val description: String, val high: Int,
+                    val low: Int)
