8e59dfd9fb02c4a5283cea043fd5dbeee9923e95
rduriancik <r.duriancik@gmail.com>
2017-04-24 22:19:40 +0200

    MainActivity - adapter initialized

--- WeatherApp/app/src/main/java/com/example/robertduriancik/weatherapp/ui/activities/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/robertduriancik/weatherapp/ui/activities/MainActivity.kt
@@ -6,9 +6,11 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.example.robertduriancik.weatherapp.R
 import com.example.robertduriancik.weatherapp.domain.commands.RequestForecastCommand
+import com.example.robertduriancik.weatherapp.domain.model.Forecast
 import com.example.robertduriancik.weatherapp.ui.adapters.ForecastListAdapter
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
@@ -23,7 +25,12 @@ class MainActivity : AppCompatActivity() {
         doAsync {
             val result = RequestForecastCommand("94043").execute();
             uiThread {
-                forecastList.adapter = ForecastListAdapter(result)
+                forecastList.adapter = ForecastListAdapter(result,
+                        object : ForecastListAdapter.OnItemClickListener {
+                            override fun invoke(forecast: Forecast) {
+                                toast(forecast.date)
+                            }
+                        })
             }
         }
     }
