fb3e115855713cedf43d452e727c5f6d48a42b4b
rduriancik <r.duriancik@gmail.com>
2017-07-02 19:11:00 +0200

    Create broadcast receiver

---
+++ GalileoX_CAAD002X_Android_App_Development_for_Beginners/BroadcastDemo/BroadcastReceiver/app/src/main/java/com/example/robert/broadcastreceiver/MyReceiver.java
@@ -0,0 +1,18 @@
+package com.example.robert.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by robert on 2.7.2017.
+ */
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String broadcastMessage = intent.getStringExtra("broadcast");
+        Toast.makeText(context, "Message: " + broadcastMessage, Toast.LENGTH_SHORT).show();
+    }
+}
