ba27554a422296e1b68bc564b3792f0582936ef1
rduriancik <r.duriancik@gmail.com>
2017-07-05 17:50:43 +0200

    Initialize recyclerView in the UpcomingFragment

--- GalileoX_CAAD002X_Android_App_Development_for_Beginners/MovieManager/app/src/main/java/com/example/robert/moviemanager/activities/MainActivity.java
+++ GalileoX_CAAD002X_Android_App_Development_for_Beginners/MovieManager/app/src/main/java/com/example/robert/moviemanager/activities/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.robert.moviemanager.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
@@ -13,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.example.robert.moviemanager.R;
 import com.example.robert.moviemanager.fragments.NowPlayingFragment;
@@ -28,15 +25,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
--- GalileoX_CAAD002X_Android_App_Development_for_Beginners/MovieManager/app/src/main/java/com/example/robert/moviemanager/fragments/UpcomingFragment.java
+++ GalileoX_CAAD002X_Android_App_Development_for_Beginners/MovieManager/app/src/main/java/com/example/robert/moviemanager/fragments/UpcomingFragment.java
@@ -3,16 +3,32 @@ package com.example.robert.moviemanager.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.robert.moviemanager.R;
+import com.example.robert.moviemanager.adapters.MovieRecyclerViewAdapter;
+import com.example.robert.moviemanager.models.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UpcomingFragment extends Fragment {
+
+    @BindView(R.id.rvMovies)
+    RecyclerView rvMovies;
+    Unbinder unbinder;
+    private List<Movie> mMovies;
 
 
     public UpcomingFragment() {
@@ -23,8 +39,32 @@ public class UpcomingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upcoming, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_upcoming, container, false);
+        unbinder = ButterKnife.bind(this, rootView);
+
+        initializeData();
+
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        rvMovies.setLayoutManager(llm);
+        rvMovies.setHasFixedSize(true);
+        MovieRecyclerViewAdapter adapter = new MovieRecyclerViewAdapter(getContext(), mMovies);
+        rvMovies.setAdapter(adapter);
+
+        return rootView;
     }
 
+
+    private void initializeData() {
+        mMovies = new ArrayList<>();
+        mMovies.add(new Movie("277834", "Rings", "Julia (Matilda Lutz) becomes worried about her boyfriend, Holt (Alex Roe) when he explores the dark urban legend of a mysterious videotape said to kill the watcher seven days after viewing. She sacrifices herself to save her boyfriend and in doing so makes a horrifying discovery: there is a \\\"movie within the movie\\\" that no one has ever seen before.", 0f, 0, "/uwNNLJULvTQfgc3PBZAY92EOJQO.jpg", "/91WPDonXsxRzi7AcfedKM3p3NFU.jpg"));
+        mMovies.add(new Movie("121856", "The Great Wall", "The story of an elite force making a last stand for humanity on the world’s most iconic structure.", 0f, 0, "/hm0Z5tpRlSzPO97U5e2Q32Y0Xrb.jpg", "/yESCAoZkaxZ2AMiHojl9jYYd9zD.jpg"));
+        mMovies.add(new Movie("330459", "Patriots Day", "An account of Boston Police Commissioner Ed Davis's actions in the events leading up to the 2013 Boston Marathon bombing and the aftermath, which includes the city-wide manhunt to find the terrorists behind it.", 0f, 0, "/gd4SDPZIdVRAxUolQ9ZCNXTkQUq.jpg", "/tiBL4PeaCPKGBz3qO4dJP2KzKop.jpg"));
+        mMovies.add(new Movie("283366", "T2 Trainspotting", "First there was an opportunity......then there was a betrayal. Twenty years have gone by. Much has changed but just as much remains the same. ", 0f, 0, "/A84pxL5z86KbV9u8GAK2mVOoXK.jpg", "/dxJynNxoG6jRttYPoXOGx7OWJvq.jpg"));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
