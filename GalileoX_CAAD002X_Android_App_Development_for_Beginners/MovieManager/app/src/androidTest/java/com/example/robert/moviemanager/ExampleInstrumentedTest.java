d2224bf9f154a1b2c1e1798c30e20ef45ab34618
rduriancik <r.duriancik@gmail.com>
2017-07-05 21:58:13 +0200

    Add a simple Espresso test

--- GalileoX_CAAD002X_Android_App_Development_for_Beginners/MovieManager/app/src/androidTest/java/com/example/robert/moviemanager/ExampleInstrumentedTest.java
+++ GalileoX_CAAD002X_Android_App_Development_for_Beginners/MovieManager/app/src/androidTest/java/com/example/robert/moviemanager/ExampleInstrumentedTest.java
@@ -2,11 +2,19 @@ package com.example.robert.moviemanager;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.robert.moviemanager.activities.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -22,5 +30,21 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.robert.moviemanager", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(
+            MainActivity.class, true, true
+    ) {
+    };
+
+    @Test
+    public void validateRecyclerViewClickSecondElement() {
+        onView(withId(R.id.rvMovies)).perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
+    }
+
+    @Test
+    public void validateRecyclerViewClickThirdElement() {
+        onView(withId(R.id.rvMovies)).perform(RecyclerViewActions.actionOnItemAtPosition(3, click()));
     }
 }
