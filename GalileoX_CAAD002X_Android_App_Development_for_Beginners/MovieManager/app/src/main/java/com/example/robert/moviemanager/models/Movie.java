5ddfbd9cf19a5e80c349aa81fd46d78b6f3f1c64
rduriancik <r.duriancik@gmail.com>
2017-07-05 14:40:52 +0200

    Movie model

---
+++ GalileoX_CAAD002X_Android_App_Development_for_Beginners/MovieManager/app/src/main/java/com/example/robert/moviemanager/models/Movie.java
@@ -0,0 +1,91 @@
+package com.example.robert.moviemanager.models;
+
+public class Movie {
+    private String id;
+    private String title;
+    private String overview;
+    private float voteAverage;
+    private float voteCount;
+    private String posterPath;
+    private String backdropPath;
+
+    public Movie(String id, String title, String overview, float voteAverage,
+                 float voteCount, String posterPath, String backdropPath) {
+        this.id = id;
+        this.title = title;
+        this.overview = overview;
+        this.voteAverage = voteAverage;
+        this.voteCount = voteCount;
+        this.posterPath = posterPath;
+        this.backdropPath = backdropPath;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public float getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(float voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public float getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(float voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", overview='" + overview + '\'' +
+                ", voteAverage=" + voteAverage +
+                ", voteCount=" + voteCount +
+                ", posterPath='" + posterPath + '\'' +
+                ", backdropPath='" + backdropPath + '\'' +
+                '}';
+    }
+}
