c0beb8b10d344fea0ee91acd58716fe0b181f1e2
rduriancik <r.duriancik@gmail.com>
2017-07-07 22:24:17 +0200

    Send an email on item click

--- GalileoX_CAAD002X_Android_App_Development_for_Beginners/HappyBirthdayApp/app/src/main/java/com/example/robert/happybirthdayapp/MainActivity.java
+++ GalileoX_CAAD002X_Android_App_Development_for_Beginners/HappyBirthdayApp/app/src/main/java/com/example/robert/happybirthdayapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.robert.happybirthdayapp;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -19,13 +21,16 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private static final String TAG = "MainActivity";
 
     private static final int READ_CONTACTS_PERMISSION_REQUEST = 1;
     private static final int CONTACT_LOADER_ID = 90;
+    private static final int CONTACT_ID_INDEX = 0;
+    private static final int LOOKUP_KEY_INDEX = 1;
 
     private SimpleCursorAdapter mAdapter;
 
@@ -48,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
         setupCursorAdapter();
         ListView lvContacts = (ListView) findViewById(R.id.lvContacts);
         lvContacts.setAdapter(mAdapter);
+        lvContacts.setOnItemClickListener(this);
 
         getPermissionToReadUserContacts();
     }
@@ -144,4 +150,55 @@ public class MainActivity extends AppCompatActivity {
             mAdapter.swapCursor(null);
         }
     };
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Cursor cursor = ((SimpleCursorAdapter) adapterView.getAdapter()).getCursor();
+        cursor.moveToPosition(i);
+
+        String contactName = cursor.getString(LOOKUP_KEY_INDEX);
+
+        Uri contactUri = ContactsContract.Contacts.getLookupUri(
+                cursor.getLong(CONTACT_ID_INDEX), contactName);
+
+        String email = getEmail(contactUri);
+
+        if (!email.isEmpty()) {
+            sendEmail(email, contactName);
+        }
+    }
+
+    private void sendEmail(String email, String contactName) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto",
+                email,
+                null
+        ));
+
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.main_email_subject));
+        intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.main_email_body, contactName));
+
+        startActivity(Intent.createChooser(intent, getString(R.string.main_email_choose)));
+    }
+
+    private String getEmail(Uri contactUri) {
+        String email = "";
+        String id = contactUri.getLastPathSegment();
+
+        Cursor cursor = getContentResolver().query(
+                ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                null,
+                ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=?",
+                new String[]{id},
+                null
+        );
+
+        int emailIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA);
+
+        if (cursor.moveToFirst()) {
+            email = cursor.getString(emailIndex);
+        }
+
+        return email;
+    }
 }
