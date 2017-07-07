8e10535df24d7aca078d9a0c1e16b5245747fb98
rduriancik <r.duriancik@gmail.com>
2017-07-07 22:40:34 +0200

    Create AboutMeActivity, add Internet permission

--- GalileoX_CAAD002X_Android_App_Development_for_Beginners/HappyBirthdayApp/app/src/main/java/com/example/robert/happybirthdayapp/MainActivity.java
+++ GalileoX_CAAD002X_Android_App_Development_for_Beginners/HappyBirthdayApp/app/src/main/java/com/example/robert/happybirthdayapp/MainActivity.java
@@ -116,7 +116,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about_me) {
+            loadAboutMe();
             return true;
         }
 
@@ -200,5 +201,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }
 
         return email;
+    }
+
+    public void loadAboutMe() {
+        Intent loadAboutMe = new Intent(MainActivity.this, AboutMeActivity.class);
+        startActivity(loadAboutMe);
     }
 }
