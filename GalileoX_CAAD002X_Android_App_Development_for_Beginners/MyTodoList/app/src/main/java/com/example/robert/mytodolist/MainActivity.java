6c976c2e27f79bdb68be2820f0d4c0ade0a9c220
rduriancik <r.duriancik@gmail.com>
2017-07-09 15:07:22 +0200

    Modify the activity_main layout, initialize a recycler view and floating action button in the MainActivity

--- GalileoX_CAAD002X_Android_App_Development_for_Beginners/MyTodoList/app/src/main/java/com/example/robert/mytodolist/MainActivity.java
+++ GalileoX_CAAD002X_Android_App_Development_for_Beginners/MyTodoList/app/src/main/java/com/example/robert/mytodolist/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.robert.mytodolist;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(itemDecoration);
+        mRecyclerView.setHasFixedSize(true);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floating_action_btn);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
     }
 }
