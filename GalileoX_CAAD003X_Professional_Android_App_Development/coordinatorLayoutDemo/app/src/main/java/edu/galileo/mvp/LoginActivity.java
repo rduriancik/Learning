ef3c3e194d81e82ff4184fb1da87a5367cc5b3c0
rduriancik <r.duriancik@gmail.com>
2017-07-13 14:06:41 +0200

    Coordinator layout demo

---
+++ GalileoX_CAAD003X_Professional_Android_App_Development/coordinatorLayoutDemo/app/src/main/java/edu/galileo/mvp/LoginActivity.java
@@ -0,0 +1,118 @@
+package edu.galileo.mvp;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity implements LoginView {
+
+    private LoginPresenter loginPresenter;
+
+    // UI references.
+    @BindView(R.id.email)
+    AutoCompleteTextView mEmailView;
+    @BindView(R.id.password)
+    EditText mPasswordView;
+    @BindView(R.id.login_form)
+    View mLoginFormView;
+    @BindView(R.id.login_progress)
+    View mProgressView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        loginPresenter = new LoginPresenterImpl(this);
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    @OnClick(R.id.email_sign_in_button)
+    public void attemptLogin() {
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        loginPresenter.validateCredentials(email, password);
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @Override
+    public void showProgress(final boolean show) {
+        int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+        mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            }
+        });
+
+        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        mProgressView.animate().setDuration(shortAnimTime).alpha(
+                show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            }
+        });
+    }
+
+    @Override
+    public void setUsernameError(int messageResId) {
+        mEmailView.setError(getString(messageResId));
+        mEmailView.requestFocus();
+    }
+
+    @Override
+    public void setPasswordError(int messageResId) {
+        mPasswordView.setError(getString(messageResId));
+        mPasswordView.requestFocus();
+    }
+
+    @Override
+    public void successAction() {
+        Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.show_snackbar)
+    public void onViewClicked() {
+        Snackbar.make(mLoginFormView,
+                "This is a simple Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("CLOSE", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Custom action
+                    }
+                }).show();
+    }
+}
+
