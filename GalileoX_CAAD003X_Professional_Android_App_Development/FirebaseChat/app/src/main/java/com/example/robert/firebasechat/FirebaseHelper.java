c91cbad2566d2f18606a866dbb0e32ad4251c2a5
rduriancik <r.duriancik@gmail.com>
2017-07-17 15:50:25 +0200

    Create a FirebaseHelper class

---
+++ GalileoX_CAAD003X_Professional_Android_App_Development/FirebaseChat/app/src/main/java/com/example/robert/firebasechat/FirebaseHelper.java
@@ -0,0 +1,63 @@
+package com.example.robert.firebasechat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by robert on 14.7.2017.
+ */
+
+public class FirebaseHelper {
+
+    private DatabaseReference dataReference;
+
+    private final static String SEPARATOR = "___";
+    private final static String CHATS_PATH = "chats";
+    private final static String USERS_PATH = "users";
+    public final static String CONTACTS_PATH = "contacts";
+
+    private static final class SingletonHolder {
+        private static final FirebaseHelper INSTANCE = new FirebaseHelper();
+    }
+
+    public static FirebaseHelper getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    private FirebaseHelper() {
+        dataReference = FirebaseDatabase.getInstance().getReference();
+    }
+
+    public DatabaseReference getDataReference() {
+        return dataReference;
+    }
+
+    public DatabaseReference getMyUserReference() {
+        return getUserReference(getAuthUserEmail());
+    }
+
+    private String getAuthUserEmail() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String email = null;
+
+        if (user != null) {
+            email = user.getEmail();
+        }
+
+        return email;
+    }
+
+    private DatabaseReference getUserReference(String email) {
+        DatabaseReference userReference = null;
+
+        if (email != null) {
+            String emailKey = email.replace(".", "_");
+            userReference = dataReference.getRoot().child(USERS_PATH).child(emailKey);
+        }
+
+        return userReference;
+    }
+
+}
