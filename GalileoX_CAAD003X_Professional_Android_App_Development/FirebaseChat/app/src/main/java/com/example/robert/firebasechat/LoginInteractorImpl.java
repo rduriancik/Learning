9d956d9f376dd02f782a9d0081eb15d86388985f
rduriancik <r.duriancik@gmail.com>
2017-07-17 15:16:55 +0200

    Implement the LoginInteractor interface

--- GalileoX_CAAD003X_Professional_Android_App_Development/FirebaseChat/app/src/main/java/com/example/robert/firebasechat/LoginInteractorImpl.java
+++ GalileoX_CAAD003X_Professional_Android_App_Development/FirebaseChat/app/src/main/java/com/example/robert/firebasechat/LoginInteractorImpl.java
@@ -14,16 +14,16 @@ class LoginInteractorImpl implements LoginInteractor {
 
     @Override
     public void checkAlreadyAuthenticated() {
-
+        loginRepository.checkAlreadyAuthenticated();
     }
 
     @Override
     public void doSignUp(String email, String password) {
-
+        loginRepository.signUp(email, password);
     }
 
     @Override
     public void doSignIn(String email, String password) {
-
+        loginRepository.signIn(email, password);
     }
 }
