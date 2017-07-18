2d2318ada340a2ea2b8142fd58c261431f969996
rduriancik <r.duriancik@gmail.com>
2017-07-18 15:21:06 +0200

    Create an OnItemClickListener interface

---
+++ GalileoX_CAAD003X_Professional_Android_App_Development/FirebaseChat/app/src/main/java/com/example/robert/firebasechat/OnItemClickListener.java
@@ -0,0 +1,11 @@
+package com.example.robert.firebasechat;
+
+/**
+ * Created by robert on 18.7.2017.
+ */
+
+public interface OnItemClickListener {
+    void onItemClick(User user);
+
+    void onItemLongClick(User user);
+}
