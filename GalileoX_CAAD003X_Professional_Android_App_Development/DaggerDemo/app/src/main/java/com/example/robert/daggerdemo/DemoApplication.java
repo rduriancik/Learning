812f32d17c688d00a32a65189e9225df6864d433
rduriancik <r.duriancik@gmail.com>
2017-07-23 12:05:52 +0200

    Create a demo application class

---
+++ GalileoX_CAAD003X_Professional_Android_App_Development/DaggerDemo/app/src/main/java/com/example/robert/daggerdemo/DemoApplication.java
@@ -0,0 +1,38 @@
+package com.example.robert.daggerdemo;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.example.robert.daggerdemo.data.DataManager;
+
+import javax.inject.Inject;
+
+/**
+ * Created by robert on 23.7.2017.
+ */
+
+public class DemoApplication extends Application {
+
+    protected ApplicationComponent applicationComponent;
+
+    @Inject
+    DataManager dataManager;
+
+    public static DemoApplication get(Context context) {
+        return (DemoApplication) context.getApplicationContext();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        applicationComponent = DaggerApplicationComponent
+                .builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+        applicationComponent.inject(this);
+    }
+
+    public ApplicationComponent getComponent() {
+        return applicationComponent;
+    }
+}
