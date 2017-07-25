72d4f0c583ce81eef5c1b0841d2425cbbba7feca
rduriancik <r.duriancik@gmail.com>
2017-07-25 16:26:34 +0200

    Cleat backstack when starting the MainActivity from the LoginActivity

--- GalileoX_CAAD003X_Professional_Android_App_Development/TwitterClient/app/src/main/java/com/example/robert/twitterclient/LoginActivity.java
+++ GalileoX_CAAD003X_Professional_Android_App_Development/TwitterClient/app/src/main/java/com/example/robert/twitterclient/LoginActivity.java
@@ -53,7 +53,11 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void navigateToMainScreen() {
-        startActivity(new Intent(this, MainActivity.class));
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
     @Override
