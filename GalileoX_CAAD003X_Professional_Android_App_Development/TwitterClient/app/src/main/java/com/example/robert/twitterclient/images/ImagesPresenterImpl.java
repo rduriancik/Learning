29951549f9cb9630ededfbd37b473a9a03e6213d
rduriancik <r.duriancik@gmail.com>
2017-07-26 22:03:37 +0200

    Implement the ImagesPresenter interface and the ImagesEvent class

---
+++ GalileoX_CAAD003X_Professional_Android_App_Development/TwitterClient/app/src/main/java/com/example/robert/twitterclient/images/ImagesPresenterImpl.java
@@ -0,0 +1,65 @@
+package com.example.robert.twitterclient.images;
+
+import com.example.robert.twitterclient.images.events.ImagesEvent;
+import com.example.robert.twitterclient.images.ui.ImagesView;
+import com.example.robert.twitterclient.libs.base.EventBus;
+
+import org.greenrobot.eventbus.Subscribe;
+
+/**
+ * Created by robert on 26.7.2017.
+ */
+
+public class ImagesPresenterImpl implements ImagesPresenter {
+
+    private EventBus eventBus;
+    private ImagesView view;
+    private ImagesInteractor interactor;
+
+    public ImagesPresenterImpl(EventBus eventBus, ImagesView view, ImagesInteractor interactor) {
+        this.eventBus = eventBus;
+        this.view = view;
+        this.interactor = interactor;
+    }
+
+    @Override
+    public void onResume() {
+        eventBus.register(this);
+    }
+
+    @Override
+    public void onPause() {
+        eventBus.unregister(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        view = null;
+    }
+
+    @Override
+    public void getImageTweets() {
+        if (view != null) {
+            view.hideImages();
+            view.showProgress();
+        }
+    }
+
+    @Override
+    @Subscribe
+    public void onEventMainThread(ImagesEvent event) {
+        String error = event.getError();
+        if (view != null) {
+            view.showImages();
+            view.hideProgress();
+
+            if (error != null) {
+                view.onError(error);
+            } else {
+                view.setContent(event.getImages());
+            }
+        }
+
+
+    }
+}
--- GalileoX_CAAD003X_Professional_Android_App_Development/TwitterClient/app/src/main/java/com/example/robert/twitterclient/images/events/ImagesEvent.java
+++ GalileoX_CAAD003X_Professional_Android_App_Development/TwitterClient/app/src/main/java/com/example/robert/twitterclient/images/events/ImagesEvent.java
@@ -1,8 +1,31 @@
 package com.example.robert.twitterclient.images.events;
 
+import com.example.robert.twitterclient.entities.Image;
+
+import java.util.List;
+
 /**
  * Created by robert on 25.7.2017.
  */
 
 public class ImagesEvent {
+
+    private String error;
+    private List<Image> images;
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public List<Image> getImages() {
+        return images;
+    }
+
+    public void setImages(List<Image> images) {
+        this.images = images;
+    }
 }
