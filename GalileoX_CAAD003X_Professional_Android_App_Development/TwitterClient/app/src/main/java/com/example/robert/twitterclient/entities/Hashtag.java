64769cf3576e78b49612fa53e93c0f88b12bb93f
rduriancik <r.duriancik@gmail.com>
2017-07-27 20:15:49 +0200

    Create a hashtag entity

---
+++ GalileoX_CAAD003X_Professional_Android_App_Development/TwitterClient/app/src/main/java/com/example/robert/twitterclient/entities/Hashtag.java
@@ -0,0 +1,47 @@
+package com.example.robert.twitterclient.entities;
+
+import java.util.List;
+
+/**
+ * Created by robert on 27.7.2017.
+ */
+
+public class Hashtag {
+    private String id;
+    private String tweetText;
+    private int favoriteCount;
+    private List<String> hashtags;
+    private final static String BASE_TWEET_URL = "https://twitter.com/null/status";
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTweetText() {
+        return tweetText;
+    }
+
+    public void setTweetText(String tweetText) {
+        this.tweetText = tweetText;
+    }
+
+    public int getFavoriteCount() {
+        return favoriteCount;
+    }
+
+    public void setFavoriteCount(int favoriteCount) {
+        this.favoriteCount = favoriteCount;
+    }
+
+    public List<String> getHashtags() {
+        return hashtags;
+    }
+
+    public void setHashtags(List<String> hashtags) {
+        this.hashtags = hashtags;
+    }
+}
