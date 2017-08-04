ff45f5cc72a390df78a26940bb3c851b813b24a8
rduriancik <r.duriancik@gmail.com>
2017-08-04 11:47:55 +0200

    Create LibsModule for the dependency injection

---
+++ GalileoX_CAAD003X_Professional_Android_App_Development/FacebookRecipes/app/src/main/java/com/example/robert/facebookrecipes/libs/di/LibsModule.java
@@ -0,0 +1,59 @@
+package com.example.robert.facebookrecipes.libs.di;
+
+import android.app.Activity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.example.robert.facebookrecipes.libs.GlideImageLoader;
+import com.example.robert.facebookrecipes.libs.GreenRobotEventBus;
+import com.example.robert.facebookrecipes.libs.base.EventBus;
+import com.example.robert.facebookrecipes.libs.base.ImageLoader;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by robert on 4.8.2017.
+ */
+
+@Module
+public class LibsModule {
+
+    private Activity activity;
+
+    public LibsModule(Activity fragment) {
+        this.activity = fragment;
+    }
+
+    @Provides
+    @Singleton
+    ImageLoader provideImageLoader(RequestManager requestManager) {
+        return new GlideImageLoader(requestManager);
+    }
+
+    @Provides
+    @Singleton
+    RequestManager provideRequestManager(Activity activity) {
+        return Glide.with(activity);
+    }
+
+    @Provides
+    @Singleton
+    Activity provideActivity() {
+        return this.activity;
+    }
+
+    @Provides
+    @Singleton
+    EventBus provideEventBus(org.greenrobot.eventbus.EventBus eventBus) {
+        return new GreenRobotEventBus(eventBus);
+    }
+
+    @Provides
+    @Singleton
+    org.greenrobot.eventbus.EventBus provideLibraryEventBus() {
+        return org.greenrobot.eventbus.EventBus.getDefault();
+    }
+}
