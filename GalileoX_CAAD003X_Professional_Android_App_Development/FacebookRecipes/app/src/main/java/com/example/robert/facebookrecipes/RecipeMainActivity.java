070927fdfde11e79227b2a3c1170952f758980e9
rduriancik <r.duriancik@gmail.com>
2017-08-06 18:18:40 +0200

    Implement a LoginActivity

--- GalileoX_CAAD003X_Professional_Android_App_Development/FacebookRecipes/app/src/main/java/com/example/robert/facebookrecipes/RecipeMainActivity.java
+++ GalileoX_CAAD003X_Professional_Android_App_Development/FacebookRecipes/app/src/main/java/com/example/robert/facebookrecipes/RecipeMainActivity.java
@@ -3,11 +3,11 @@ package com.example.robert.facebookrecipes;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class RecipeMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_recipe_main);
     }
 }
