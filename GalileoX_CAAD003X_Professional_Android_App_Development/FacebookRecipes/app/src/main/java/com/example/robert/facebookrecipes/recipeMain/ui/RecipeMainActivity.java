bf8c47e90fd6fba4fe8d509ccafdff841d3acc93
rduriancik <r.duriancik@gmail.com>
2017-08-17 23:38:50 +0200

    Create SwipeGestureDetector class. Add onDismiss and onKeep animations

--- GalileoX_CAAD003X_Professional_Android_App_Development/FacebookRecipes/app/src/main/java/com/example/robert/facebookrecipes/recipeMain/ui/RecipeMainActivity.java
+++ GalileoX_CAAD003X_Professional_Android_App_Development/FacebookRecipes/app/src/main/java/com/example/robert/facebookrecipes/recipeMain/ui/RecipeMainActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -26,7 +30,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class RecipeMainActivity extends AppCompatActivity implements RecipeMainView {
+public class RecipeMainActivity extends AppCompatActivity implements RecipeMainView, SwipeGestureListener {
 
     @BindView(R.id.imgRecipe)
     ImageView imgRecipe;
@@ -52,6 +56,7 @@ public class RecipeMainActivity extends AppCompatActivity implements RecipeMainV
 
         setupInjection();
         setupImageLoader();
+        setupGestureDetector();
         presenter.onCreate();
         presenter.getNextRecipe();
     }
@@ -116,6 +121,18 @@ public class RecipeMainActivity extends AppCompatActivity implements RecipeMainV
         imageLoader.setOnFinishedImageLoadingListener(glideRequestListener);
     }
 
+    private void setupGestureDetector() {
+        final GestureDetector gestureDetector = new GestureDetector(this, new SwipeGestureDetector(this));
+        View.OnTouchListener gestOnTouchListener = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }
+        };
+
+        imgRecipe.setOnTouchListener(gestOnTouchListener);
+    }
+
     @Override
     public void showProgress() {
         progressBar.setVisibility(View.VISIBLE);
@@ -138,17 +155,45 @@ public class RecipeMainActivity extends AppCompatActivity implements RecipeMainV
         imgDismiss.setVisibility(View.GONE);
     }
 
+    private void clearImage() {
+        imgRecipe.setImageResource(0);
+    }
+
     @Override
     public void saveAnimation() {
-
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.save_animation);
+        animation.setAnimationListener(getAnimationListener());
+        imgRecipe.startAnimation(animation);
     }
 
     @Override
     public void dismissAnimation() {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.dismiss_animation);
+        animation.setAnimationListener(getAnimationListener());
+        imgRecipe.startAnimation(animation);
+    }
 
+    private Animation.AnimationListener getAnimationListener() {
+        return new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                clearImage();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        };
     }
 
     @OnClick(R.id.imgKeep)
+    @Override
     public void onKeep() {
         if (currentRecipe != null) {
             presenter.saveRecipe(currentRecipe);
@@ -156,6 +201,7 @@ public class RecipeMainActivity extends AppCompatActivity implements RecipeMainV
     }
 
     @OnClick(R.id.imgDismiss)
+    @Override
     public void onDismiss() {
         presenter.dismissRecipe();
     }
---
+++ GalileoX_CAAD003X_Professional_Android_App_Development/FacebookRecipes/app/src/main/java/com/example/robert/facebookrecipes/recipeMain/ui/SwipeGestureListener.java
@@ -0,0 +1,11 @@
+package com.example.robert.facebookrecipes.recipeMain.ui;
+
+/**
+ * Created by robert on 17.8.2017.
+ */
+
+interface SwipeGestureListener {
+    void onKeep();
+
+    void onDismiss();
+}
