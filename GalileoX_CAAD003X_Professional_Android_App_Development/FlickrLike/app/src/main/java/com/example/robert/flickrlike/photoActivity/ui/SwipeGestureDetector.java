ede1cd27d10c302a383543d82518b7bf9933303e
rduriancik <r.duriancik@gmail.com>
2017-08-21 11:57:48 +0200

    Create SwipeGestureDetector and SwipeGestureListener

---
+++ GalileoX_CAAD003X_Professional_Android_App_Development/FlickrLike/app/src/main/java/com/example/robert/flickrlike/photoActivity/ui/SwipeGestureDetector.java
@@ -0,0 +1,37 @@
+package com.example.robert.flickrlike.photoActivity.ui;
+
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+/**
+ * Created by robert on 21.8.2017.
+ */
+
+public class SwipeGestureDetector extends GestureDetector.SimpleOnGestureListener {
+    private static final int SWIPE_THRESHOLD = 100;
+    private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+    private SwipeGestureListener gestureListener;
+
+    public SwipeGestureDetector(SwipeGestureListener gestureListener) {
+        this.gestureListener = gestureListener;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        float diffX = e2.getX() - e1.getX();
+        float diffY = e2.getY() - e1.getY();
+
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                gestureListener.onSave();
+            }
+        } else {
+            if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                gestureListener.onDismiss();
+            }
+        }
+
+        return super.onFling(e1, e2, velocityX, velocityY);
+    }
+}
---
+++ GalileoX_CAAD003X_Professional_Android_App_Development/FlickrLike/app/src/main/java/com/example/robert/flickrlike/photoActivity/ui/SwipeGestureListener.java
@@ -0,0 +1,11 @@
+package com.example.robert.flickrlike.photoActivity.ui;
+
+/**
+ * Created by robert on 21.8.2017.
+ */
+
+public interface SwipeGestureListener {
+    void onSave();
+
+    void onDismiss();
+}
