cfba7ee602f6571148f785ffe3f597da6dc5a51d
rduriancik <r.duriancik@gmail.com>
2017-08-31 23:44:15 +0200

    Start working on the MainActivity

---
+++ GalileoX_CAAD003X_Professional_Android_App_Development/PhotoFeed/app/src/main/java/com/example/robert/photofeed/main/MainActivity.java
@@ -0,0 +1,173 @@
+package com.example.robert.photofeed.main;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.robert.photofeed.PhotoFeedApp;
+import com.example.robert.photofeed.R;
+import com.example.robert.photofeed.login.ui.LoginActivity;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements
+        MainView, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.tabs)
+    TabLayout tabs;
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
+
+    MainPresenter presenter;
+    MainSectionsPagerAdapter adapter;
+    SharedPreferences sharedPreferences;
+
+    private String photoPath;
+    private Location lastLocation;
+    private GoogleApiClient googleApiClient;
+    private final static int REQUEST_PICTURE = 0;
+    private final static int PERMISSIONS_REQUEST_READ_MEDIA = 10;
+    private final static int PRERMISSIONS_REQUEST_LOCATION = 11;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setupInjection();
+        setupNavigation();
+        setupGoogleApiClient();
+
+        presenter.onCreate();
+    }
+
+    @Override
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupGoogleApiClient() {
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    private void setupNavigation() {
+        PhotoFeedApp app = (PhotoFeedApp) getApplication();
+        String email = sharedPreferences.getString(app.getEmailKey(), "");
+        toolbar.setTitle(email);
+        setSupportActionBar(toolbar);
+
+        viewPager.setAdapter(adapter);
+        tabs.setupWithViewPager(viewPager);
+    }
+
+    private void setupInjection() {
+
+    }
+
+    private void checkLocationPermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int permissionLocation = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PRERMISSIONS_REQUEST_LOCATION);
+        } else {
+            getLastKnownLocation();
+        }
+    }
+
+    private void logout() {
+        presenter.logout();
+        sharedPreferences.edit().clear().apply();
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onUploadInit() {
+        Snackbar.make(viewPager, R.string.main_notice_upload_init, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUploadComplete() {
+        Snackbar.make(viewPager, R.string.main_notice_upload_complete, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUploadError(String error) {
+        Snackbar.make(viewPager, error, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        checkLocationPermission();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Snackbar.make(viewPager, connectionResult.getErrorMessage(), Snackbar.LENGTH_SHORT).show();
+    }
+}
