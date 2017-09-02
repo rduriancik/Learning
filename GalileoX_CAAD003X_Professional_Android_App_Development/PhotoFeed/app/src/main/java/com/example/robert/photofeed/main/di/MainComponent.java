a66a51fde955230228a32a8030a9a049b70fd9fc
rduriancik <r.duriancik@gmail.com>
2017-09-02 12:14:31 +0200

    Create MainComponent and MainModule for the MainActivity

---
+++ GalileoX_CAAD003X_Professional_Android_App_Development/PhotoFeed/app/src/main/java/com/example/robert/photofeed/main/di/MainComponent.java
@@ -0,0 +1,17 @@
+package com.example.robert.photofeed.main.di;
+
+import com.example.robert.photofeed.libs.di.LibsModule;
+import com.example.robert.photofeed.main.ui.MainActivity;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by robert on 2.9.2017.
+ */
+@Singleton
+@Component(modules = {MainModule.class, DomainModule.class, LibsModule.class, PhotoFeedAppModule.class})
+public interface MainComponent {
+    void inject(MainActivity activity);
+}
