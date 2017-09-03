acb36a4a667f0f5de60e50036e11bc78d8943909
rduriancik <r.duriancik@gmail.com>
2017-09-03 21:26:53 +0200

    Observable.just() method, create observer and action object

--- RxAndroidDemo/app/src/main/java/com/example/robert/rxandroiddemo/MainActivity.java
+++ RxAndroidDemo/app/src/main/java/com/example/robert/rxandroiddemo/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.robert.rxandroiddemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<String> myObservable = Observable.just("Hello");
+
+        Observer<String> myObserver = new Observer<String>() {
+            @Override
+            public void onCompleted() {
+                Log.d("RXResult", "No more data to emit");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d("RXResult", s);
+            }
+        };
+
+        Subscription subscription1 = myObservable.subscribe(myObserver);
+
+        Action1<String> myAction = new Action1<String>() {
+            @Override
+            public void call(String o) {
+                Log.d("RXResult", "The result from this inner class Action is: " + o);
+            }
+        };
+
+        Subscription subscription2 = myObservable.subscribe(myAction);
     }
 }
