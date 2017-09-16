6391af93aa3443d4fef18a02ca850eac883816e6
rduriancik <r.duriancik@gmail.com>
2017-09-16 16:24:11 +0200

    1.Refactoring/ GetFavoritesUseCase

---
+++ Catter2-Dagger2Sample/app/src/main/java/io/catter2/favorites/GetFavoritesUseCase.java
@@ -0,0 +1,46 @@
+package io.catter2.favorites;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by robert on 14.9.2017.
+ */
+
+public class GetFavoritesUseCase {
+    public interface Callback {
+        void favoriteUrlsUpdated(List<String> favoriteUrls);
+    }
+
+    private FavoritesRepository repository;
+
+    public GetFavoritesUseCase(Context context, String userToken) {
+        repository = new FavoritesRepository(context, userToken);
+    }
+
+    public void getFavorites(final Callback callback) {
+        callback.favoriteUrlsUpdated(favoritesToUrls(repository.getFavorites()));
+
+        repository.registerOnChangeListener(new FavoritesRepository.OnChangeListener() {
+            @Override
+            public void onChanged(List<FavoriteModel> favorites) {
+                callback.favoriteUrlsUpdated(favoritesToUrls(favorites));
+            }
+        });
+    }
+
+    public void clear() {
+        repository.clearOnChangeListener();
+    }
+
+    private List<String> favoritesToUrls(List<FavoriteModel> favorites) {
+        ArrayList<String> urls = new ArrayList<>(favorites.size());
+        for (FavoriteModel favorite : favorites) {
+            urls.add(favorite.getUrl());
+        }
+
+        return urls;
+    }
+}
