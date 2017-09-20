05b2c9f862e1d2b19c0c996a62d3b76ed9fed60e
rduriancik <r.duriancik@gmail.com>
2017-09-20 20:01:25 +0200

    5.Refactoring/ FavoritesActivityTest

---
+++ Catter2-Dagger2Sample/app/src/androidTest/java/io/catter2/FavoritesActivityTest.java
@@ -0,0 +1,68 @@
+package io.catter2;
+
+import android.content.Intent;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.catter2.favorites.FavoritesRepository;
+import io.catter2.favorites.GetFavoritesUseCase;
+import io.catter2.favorites_activity.FavoritesActivity;
+import io.catter2.favorites_activity.FavoritesActivityModule;
+
+import static android.support.test.espresso.Espresso.onView;
+
+/**
+ * Created by robert on 20.9.2017.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class FavoritesActivityTest {
+    @Rule
+    public ActivityTestRule<FavoritesActivity> activityRule =
+            new ActivityTestRule<FavoritesActivity>(FavoritesActivity.class, true, false);
+
+    @Test
+    public void testThereAreThreeImages() {
+        final List<String> expectedUrls = new ArrayList<>();
+        expectedUrls.add("url10");
+        expectedUrls.add("url11");
+        expectedUrls.add("url12");
+        setupGetFavoritesResponse(expectedUrls);
+
+        activityRule.launchActivity(new Intent());
+        onView(ViewMatchers.withId(R.id.favorites_rv)).check(new RecyclerViewItemCountAssertion(3));
+    }
+
+    @Test
+    public void testZeroImages() {
+        final List<String> expectedUrls = new ArrayList<>();
+        setupGetFavoritesResponse(expectedUrls);
+
+        activityRule.launchActivity(new Intent());
+        onView(ViewMatchers.withId(R.id.favorites_rv)).check(new RecyclerViewItemCountAssertion(0));
+    }
+
+    private void setupGetFavoritesResponse(final List<String> urls) {
+        FavoritesActivityModule.testGetFavoritesUseCase =
+                new GetFavoritesUseCase(Mockito.mock(FavoritesRepository.class)) {
+                    @Override
+                    public void getFavorites(Callback callback) {
+                        callback.favoriteUrlsUpdated(urls);
+                    }
+
+                    @Override
+                    public void clear() {
+                        //
+                    }
+                };
+    }
+}
