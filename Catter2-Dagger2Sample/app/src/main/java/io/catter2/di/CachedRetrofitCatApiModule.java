c9323d3aa7ab96eb937a48a39aaaacbb5fa452a5
rduriancik <r.duriancik@gmail.com>
2017-09-20 11:14:41 +0200

    4.Refactoring/ CachedRetrofitCatApiModule, TheCatAPIModule

---
+++ Catter2-Dagger2Sample/app/src/main/java/io/catter2/di/CachedRetrofitCatApiModule.java
@@ -0,0 +1,25 @@
+package io.catter2.di;
+
+import io.catter2.cat_api.CacheTheCatAPI;
+import io.catter2.cat_api.RetrofitTheCatAPI;
+import io.catter2.cat_api.TheCatAPI;
+
+/**
+ * Created by robert on 20.9.2017.
+ */
+
+public class CachedRetrofitCatApiModule implements TheCatAPIModule {
+
+    @Override
+    public TheCatAPI provideTheCatAPI() {
+        return provideTheCatAPICached(provideTheCatAPIRetrofit());
+    }
+
+    public TheCatAPI provideTheCatAPIRetrofit() {
+        return new RetrofitTheCatAPI();
+    }
+
+    public TheCatAPI provideTheCatAPICached(TheCatAPI api) {
+        return new CacheTheCatAPI(api);
+    }
+}
---
+++ Catter2-Dagger2Sample/app/src/main/java/io/catter2/di/TheCatAPIModule.java
@@ -0,0 +1,11 @@
+package io.catter2.di;
+
+import io.catter2.cat_api.TheCatAPI;
+
+/**
+ * Created by robert on 20.9.2017.
+ */
+
+public interface TheCatAPIModule {
+    TheCatAPI provideTheCatAPI();
+}
