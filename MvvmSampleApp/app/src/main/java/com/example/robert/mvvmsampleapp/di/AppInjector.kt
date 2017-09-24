e9e589b6688d986ce730e920127daa5a88a5947c
rduriancik <r.duriancik@gmail.com>
2017-09-24 22:59:18 +0200

    Create AppInjector

---
+++ MvvmSampleApp/app/src/main/java/com/example/robert/mvvmsampleapp/di/AppInjector.kt
@@ -0,0 +1,62 @@
+package com.example.robert.mvvmsampleapp.di
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import android.support.v4.app.FragmentManager
+import com.example.robert.mvvmsampleapp.MVVMApplication
+import dagger.android.AndroidInjection
+import dagger.android.support.AndroidSupportInjection
+import dagger.android.support.HasSupportFragmentInjector
+
+/**
+ * Created by robert on 23.9.2017.
+ */
+fun init(application: MVVMApplication) {
+    DaggerAppComponent.builder()
+            .application(application)
+            .build()
+            .inject(application)
+
+    application.registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
+        override fun onActivityPaused(activity: Activity?) {
+        }
+
+        override fun onActivityResumed(activity: Activity?) {
+        }
+
+        override fun onActivityStarted(activity: Activity?) {
+        }
+
+        override fun onActivityDestroyed(activity: Activity?) {
+        }
+
+        override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
+        }
+
+        override fun onActivityStopped(activity: Activity?) {
+        }
+
+        override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+            activity?.let { handleActivity(it) }
+        }
+    })
+}
+
+private fun handleActivity(activity: Activity) {
+    when (activity) {
+        is HasSupportFragmentInjector -> AndroidInjection.inject(activity)
+        is FragmentActivity -> {
+            activity.supportFragmentManager.registerFragmentLifecycleCallbacks(
+                    object : FragmentManager.FragmentLifecycleCallbacks() {
+                        override fun onFragmentCreated(fm: FragmentManager?, f: Fragment?, savedInstanceState: Bundle?) {
+                            if (f != null && f is Injectable) {
+                                AndroidSupportInjection.inject(f)
+                            }
+                        }
+                    }, true)
+        }
+    }
+}
---
+++ MvvmSampleApp/app/src/main/java/com/example/robert/mvvmsampleapp/di/Injectable.kt
@@ -0,0 +1,7 @@
+package com.example.robert.mvvmsampleapp.di
+
+/**
+ * Created by robert on 23.9.2017.
+ */
+interface Injectable {
+}
