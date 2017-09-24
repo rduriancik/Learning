98287fd7aa39eab15dff45aef63221c274792e73
rduriancik <r.duriancik@gmail.com>
2017-09-24 23:20:54 +0200

    Adjust ProjectViewModel

--- MvvmSampleApp/app/src/main/java/com/example/robert/mvvmsampleapp/viewmodel/ProjectViewModel.kt
+++ MvvmSampleApp/app/src/main/java/com/example/robert/mvvmsampleapp/viewmodel/ProjectViewModel.kt
@@ -2,29 +2,50 @@ package com.example.robert.mvvmsampleapp.viewmodel
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
-import android.arch.lifecycle.ViewModel
-import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
 import android.databinding.ObservableField
+import android.util.Log
 import com.example.robert.mvvmsampleapp.service.model.Project
 import com.example.robert.mvvmsampleapp.service.repository.ProjectRepository
+import javax.inject.Inject
 
 /**
  * Created by robert on 13.9.2017.
  */
-class ProjectViewModel(application: Application, val projectId: String) : AndroidViewModel(application) {
+class ProjectViewModel @Inject constructor(projectRepository: ProjectRepository, application: Application)
+    : AndroidViewModel(application) {
 
-    val projectObservable = ProjectRepository().getProjectDetails("Google", projectId)
+    companion object {
+        private const val TAG: String = "ProjectViewModel"
+        private val ABSENT = MutableLiveData<Project>()
+
+    }
+
+    val projectObservable: LiveData<Project>
+    private val projectID: MutableLiveData<String>
     val project = ObservableField<Project>()
+
+    init {
+        ProjectViewModel.ABSENT.value = null
+        this.projectID = MutableLiveData<String>()
+        projectObservable = Transformations.switchMap(projectID) { input ->
+            if (input.isEmpty()) {
+                Log.i(TAG, "ProjectViewModel projectID is absent")
+                ABSENT
+            } else {
+                Log.i(TAG, "ProjectViewModel projectID is ${projectID.value}")
+                projectRepository.getProjectDetails("Google", projectID.value!!)
+            }
+        }
+    }
 
     fun setProject(project: Project) {
         this.project.set(project)
     }
 
-    /**
-     * A creator is used to inject the project ID into the ViewModel
-     */
-    class Factory(private val application: Application, private val projectId: String) : ViewModelProvider.Factory {
-        override fun <T : ViewModel?> create(modelClass: Class<T>?): T =
-                ProjectViewModel(application, projectId) as T
+    fun setProjectID(projectID: String) {
+        this.projectID.value = projectID
     }
 }
