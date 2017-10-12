b0fc0ee3e359313eb4a7238a0281a65c39f29f63
rduriancik <r.duriancik@gmail.com>
2017-10-12 19:34:30 +0200

    Create ViewType and ViewTypeDelegateAdapter interfaces, create AdapterConstants

---
+++ KedditMvvm/app/src/main/java/com/example/robert/kedditmvvm/common/adapter/AdapterConstants.kt
@@ -0,0 +1,9 @@
+package com.example.robert.kedditmvvm.common.adapter
+
+/**
+ * Created by robert on 12.10.2017.
+ */
+object AdapterConstants {
+    val NEWS = 1
+    val LOADING = 1
+}
---
+++ KedditMvvm/app/src/main/java/com/example/robert/kedditmvvm/common/adapter/ViewType.kt
@@ -0,0 +1,8 @@
+package com.example.robert.kedditmvvm.common.adapter
+
+/**
+ * Created by robert on 12.10.2017.
+ */
+interface ViewType {
+    fun getViewType(): Int
+}
