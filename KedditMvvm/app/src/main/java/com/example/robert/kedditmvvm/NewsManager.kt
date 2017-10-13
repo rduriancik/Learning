29a6ddbcab037688c521d7c8397198b16b2647d7
rduriancik <r.duriancik@gmail.com>
2017-10-13 19:25:29 +0200

    Use the RestAPI to fetch data in the NewsManager class

--- KedditMvvm/app/src/main/java/com/example/robert/kedditmvvm/NewsManager.kt
+++ KedditMvvm/app/src/main/java/com/example/robert/kedditmvvm/NewsManager.kt
@@ -1,27 +1,29 @@
 package com.example.robert.kedditmvvm
 
+import com.example.robert.kedditmvvm.api.RestAPI
 import io.reactivex.Observable
 
 /**
  * Created by robert on 13.10.2017.
  */
-class NewsManager {
-    fun getNews(): Observable<List<RedditNewsItem>> =
+class NewsManager(private val api: RestAPI = RestAPI()) {
+    fun getNews(limit: String = "10"): Observable<List<RedditNewsItem>> =
             Observable.create { subscriber ->
+                val callResponse = api.getNews("", limit)
+                val response = callResponse.execute()
 
-                val news = mutableListOf<RedditNewsItem>()
-                for (i in 1..10) {
-                    news.add(RedditNewsItem(
-                            "author$i",
-                            "Title $i",
-                            i, // number of comments
-                            +1457207701L - i * 200, // time
-                            "http://lorempixel.com/200/200/technics/$i", // image url
-                            "url"
-                    ))
+                if (response.isSuccessful) {
+                    val news = response.body()?.data?.children?.map {
+                        val item = it.data
+                        RedditNewsItem(item.author, item.title, item.num_comments, item.created,
+                                item.thumbnail, item.url)
+                    } ?: emptyList<RedditNewsItem>()
 
+                    subscriber.onNext(news)
+                    subscriber.onComplete()
+
+                } else {
+                    subscriber.onError(Throwable(response.message()))
                 }
-
-                subscriber.onNext(news)
             }
 }
