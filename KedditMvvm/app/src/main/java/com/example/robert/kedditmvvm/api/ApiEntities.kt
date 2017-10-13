5195b2d2f526bda1b6c0df66f2b9578ca9060170
rduriancik <r.duriancik@gmail.com>
2017-10-13 18:59:12 +0200

    Create ApiEntities for mapping JSON response

---
+++ KedditMvvm/app/src/main/java/com/example/robert/kedditmvvm/api/ApiEntities.kt
@@ -0,0 +1,19 @@
+package com.example.robert.kedditmvvm.api
+
+/**
+ * Created by robert on 13.10.2017.
+ */
+class RedditNewsResponse(val data: RedditDataResponse)
+
+class RedditDataResponse(val children: List<RedditChildrenResponse>,
+                         val after: String?,
+                         val before: String?)
+
+class RedditChildrenResponse(val data: RedditNewsDataResponse)
+
+class RedditNewsDataResponse(val author: String,
+                             val title: String,
+                             val num_comments: Int,
+                             val created: Long,
+                             val thumbnail: String,
+                             val url: String)
