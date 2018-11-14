93c3add2700ab753d792edfd0ac0a0ce7986a69e
rduriancik <r.duriancik@gmail.com>
2018-11-14 20:41:24 +0100

    Reinitialize repo

--- Complete Python Masterclass/Playing/if_else_elif_challenge.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age, {}: ".format(name)))
-
-if 17 < age < 31:
-    print("Welcome to the club 18-30 holidays, {}".format(name))
-else:
-    print("I'm sorry, you are too young. Come back in {} years".format(18 - age))
--- Complete Python Masterclass/Playing/num_systems_challenge.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter a number: "))
-
-bin_num = ""
-orig_num = num
-while num != 0:
-    bin_num = str(num % 2) + bin_num
-    num //= 2
-
-print("Number {} in binary is {}".format(bin_num, orig_num))
