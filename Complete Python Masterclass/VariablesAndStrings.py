feea179023b2a92944c27f13e47eb652f71c80f2
rduriancik <r.duriancik@gmail.com>
2018-11-14 21:22:44 +0100

    First programs in python

---
+++ Complete Python Masterclass/VariablesAndStrings.py
@@ -0,0 +1,13 @@
+__author__= 'Robert Duriancik'
+a = 12
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a + b / 3 - 4 * 12)
+print((((a+b) / 3) -4) * 12)
+for i in range(1, a // b):
+    print(i)
