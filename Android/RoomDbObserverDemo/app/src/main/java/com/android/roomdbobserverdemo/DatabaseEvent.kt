79ad63a11582d8427ecd73274314bcb96f0f3e9e
rduriancik <r.duriancik@gmail.com>
2019-03-03 13:03:22 +0100

    Create DatabaseEvent and DatabaseEventType enum class.

---
+++ Android/RoomDbObserverDemo/app/src/main/java/com/android/roomdbobserverdemo/DatabaseEvent.kt
@@ -0,0 +1,6 @@
+package com.android.roomdbobserverdemo
+
+/**
+ * Created by Robert Duriancik on 2/25/19.
+ */
+data class DatabaseEvent<T>(val eventType: DatabaseEventType, val value: T?)
---
+++ Android/RoomDbObserverDemo/app/src/main/java/com/android/roomdbobserverdemo/DatabaseEventType.kt
@@ -0,0 +1,10 @@
+package com.android.roomdbobserverdemo
+
+/**
+ * Created by Robert Duriancik on 2/25/19.
+ */
+enum class DatabaseEventType {
+    INSERTED,
+    UPDATED,
+    REMOVED
+}
