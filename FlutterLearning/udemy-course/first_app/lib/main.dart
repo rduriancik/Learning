b9ea796941301335caa24d846deb78f06487b833
rduriancik <r.duriancik@gmail.com>
2019-11-23 13:01:24 -0700

    Create answer widget. Improve business logic.

--- FlutterLearning/udemy-course/first_app/lib/main.dart
+++ FlutterLearning/udemy-course/first_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,42 +12,45 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': "What's your favorite color?",
+      'answers': ["Black", "Red", "Green", "White"],
+    },
+    {
+      'questionText': "What's your favorite animal?",
+      'answers': ["Rabbit", "Snake", "Elephant", "Lion"],
+    },
+    {
+      'questionText': "Who's your favorite instructor?",
+      'answers': ["Robert", "Max", "Steven", "Josh"],
+    }
+  ];
   var _questionIndex = 0;
 
   void _answerQuestion() {
-    setState(() {
-      _questionIndex += 1;
-    });
+    if (_questionIndex < _questions.length) {
+      setState(() {
+        _questionIndex += 1;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      "What's your favorite color?",
-      "What's your favorite animal?"
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My first App"),
         ),
-        body: Column(
+        body: _questionIndex < _questions.length ? Column(
           children: [
-            Question(questions[_questionIndex]),
-            RaisedButton(
-              child: Text("Answer 1"),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text("Answer 2"),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text("Answer 3"),
-              onPressed: _answerQuestion,
-            ),
+            Question(_questions[_questionIndex]['questionText']),
+            ...(_questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) => Answer(_answerQuestion, answer))
+                .toList()
           ],
-        ),
+        ) : Center(child: Text('You did it!'),),
       ),
     );
   }
