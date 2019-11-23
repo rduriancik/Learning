3c467cd0996fa799dbe33552ef75160a7504f037
rduriancik <r.duriancik@gmail.com>
2019-11-23 13:57:02 -0700

    Add button for restarting the quiz

--- FlutterLearning/udemy-course/first_app/lib/main.dart
+++ FlutterLearning/udemy-course/first_app/lib/main.dart
@@ -44,6 +44,13 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
     if (_questionIndex < _questions.length) {
       _totalScore += score;
@@ -66,7 +73,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 answerQuestion: _answerQuestion,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- FlutterLearning/udemy-course/first_app/lib/result.dart
+++ FlutterLearning/udemy-course/first_app/lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     var resultText;
@@ -22,10 +23,21 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text("Restart Quiz!"),
+            onPressed: resetHandler,
+            textColor: Colors.blue,
+          )
+        ],
       ),
     );
   }
