d7e20ff9dcea304b993797e679fa297d2ee3dfd9
rduriancik <r.duriancik@gmail.com>
2019-12-23 17:34:45 +0100

    Utilize theming

--- FlutterLearning/udemy-course/personal_expenses/lib/main.dart
+++ FlutterLearning/udemy-course/personal_expenses/lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Personal Expenses',
+      theme: ThemeData(primarySwatch: Colors.green, accentColor: Colors.yellow),
       home: MyHomePage(),
     );
   }
@@ -55,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Personal Expenses'),
         actions: <Widget>[
           IconButton(
             icon: Icon(
