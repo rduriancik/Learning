8abec8e75cdfca6ad4c78215c37ed7a63be6949e
rduriancik <r.duriancik@gmail.com>
2020-03-14 16:25:42 +0100

    Create meal item widget. Reorganize files.

---
+++ FlutterLearning/udemy-course/meals_app/lib/screens/category_meals_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+import '../widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  static const routeArgId = 'id';
+  static const routeArgTitle = 'title';
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs[routeArgId];
+    final categoryTitle = routeArgs[routeArgTitle];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          final meal = categoryMeals[index];
+          return MealItem(
+            title: meal.title,
+            imageUrl: meal.imageUrl,
+            duration: meal.duration,
+            complexity: meal.complexity,
+            affordability: meal.affordability,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
