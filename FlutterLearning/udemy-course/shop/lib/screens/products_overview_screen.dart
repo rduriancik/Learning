ad7391f01a1012ac34f20770a83096338ba96178
rduriancik <r.duriancik@gmail.com>
2020-04-07 22:33:40 +0200

    Create Cart provider and utilize it in the product overview and product item. Add shopping cart icon with a badge.

--- FlutterLearning/udemy-course/shop/lib/screens/products_overview_screen.dart
+++ FlutterLearning/udemy-course/shop/lib/screens/products_overview_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
+import '../widgets/badge.dart';
+import '../providers/cart.dart';
 
 enum FilterOptions { Favorite, All }
 
@@ -35,6 +38,18 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
                 _showOnlyFavorites = selectedValue == FilterOptions.Favorite;
               });
             },
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: ch,
+              value: cart.itemCount.toString(),
+            ),
+            child: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+              ),
+              onPressed: () {},
+            ),
           )
         ],
       ),
